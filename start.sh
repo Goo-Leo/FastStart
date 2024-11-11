f9d25f6be6a3af79298fab04112acef3c0b91bda
Goo-Leo <1063376141@qq.com>
2024-11-11 17:15:58 +0800

    change source added

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 pgm="$1"
-options="$2"
 
-case "pgm" in
+case "$pgm" in
     zsh)
 	    echo "Installing zsh"
 	    sudo apt install -y zsh
@@ -18,6 +17,27 @@ case "pgm" in
 	;;
     neovim)
 	    sudo apt install -y neovim clangd
+	;;
+    change)
+	    sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+	    sudo cat > /etc/apt/sources.list <<EOF
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble-backports main restricted universe multiverse
+
+# 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
+deb http://security.ubuntu.com/ubuntu/ noble-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ noble-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble-proposed main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble-proposed main restricted universe multiverse
+EOF
+	sudo apt-get update && sudo apt-get upgrade
 	;;
     *)
 	    echo "Not support yet, exiting"
