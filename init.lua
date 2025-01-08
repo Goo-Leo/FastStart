ee8adb4091764920f7f2f5b226b877e61c9e6378
Goo-Leo <1063376141@qq.com>
2025-01-08 10:04:21 +0800

    corrrect

--- init.lua
+++ init.lua
@@ -1,21 +1,28 @@
--- 设置行号
-vim.o.number = true  -- 等价于 set number
+vim.cmd('syntax on')
 
--- 启用语法高亮
-vim.cmd('syntax on')  -- 使用 vim.cmd 执行 Vim 脚本命令
-
--- 设置缩进宽度为 4
+vim.o.number = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
-vim.o.expandtab = true  -- 将 Tab 转换为空格
-
--- 启用鼠标支持
+vim.o.expandtab = true  
 vim.o.mouse = 'a'
 
--- 设置颜色主题
 vim.cmd('colorscheme desert')
 
+
+-- 快捷键映射
+vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+
+
+
+
+
+
+
+
 -- 自动安装 packer.nvim
+-- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 local ensure_packer = function()
     local fn = vim.fn
     local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
@@ -32,8 +39,28 @@ ensure_packer()
 -- 初始化 packer 并加载插件
 require('packer').startup(function()
     use 'wbthomason/packer.nvim'
-    use 'neovim/nvim-lspconfig'  -- LSP 配置
-    use 'nvim-treesitter/nvim-treesitter'  -- 树木语法解析器
-    -- 在这里添加更多插件
+    use 'neovim/nvim-lspconfig'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'windwp/nvim-autopairs'  
+    use 'nvim-telescope/telescope.nvim' 
+    use 'nvim-lualine/lualine.nvim'
+    use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+        'nvim-tree/nvim-web-devicons', 
+    }
+}
 end)
+
+--配置 Tree 文件目录
+require('nvim-tree').setup({
+    view = {
+        width = 30,
+        side = 'left',
+    },
+    filters = {
+        dotfiles = false,
+    },
+})
+
 
